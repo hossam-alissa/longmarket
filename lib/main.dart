13d0917091901af2262c315d33be6a42508a6f88
hossam.alissa <hossam.alissa@yahoo.com>
2021-03-04 09:09:31 +0200

    Edit main

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../screens/screens.dart';
-import '../screens/nav_screen.dart';
-import '../models/models.dart';
+import 'screens/screens.dart';
+import 'screens/nav_screen.dart';
+import 'models/models.dart';
 import 'config/config.dart';
 
 void main()  async{
