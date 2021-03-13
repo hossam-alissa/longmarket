2880352d4bb75881f2c8d063112dc3cc40e0625f
hossam.alissa <hossam.alissa@yahoo.com>
2021-03-13 12:05:44 +0200

    clean code

--- lib/config/language.dart
+++ lib/config/language.dart
@@ -2,15 +2,20 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 bool isLeft = false;
 
-setLanguage ()async{
+setLanguage() async {
   SharedPreferences preferencesLanguage = await SharedPreferences.getInstance();
   await preferencesLanguage.setBool("language", isLeft);
-
 }
-getLanguage()async{
-  SharedPreferences preferencesLanguage = await SharedPreferences.getInstance();
 
-  if(await preferencesLanguage.getBool("language") != null){
-    isLeft = await preferencesLanguage.getBool("language");
+getLanguage() async {
+  SharedPreferences preferencesLanguage = await SharedPreferences.getInstance();
+  try {
+    if (await preferencesLanguage.getBool("language") != null) {
+      isLeft = await preferencesLanguage.getBool("language");
+    }
+    print("+++ +++ Done lib-config-language-getLanguage");
+  } catch (e) {
+    print("+++ +++ Error lib-config-language-getLanguage");
+    print(e);
   }
 }
